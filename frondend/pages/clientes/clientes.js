d7b997ce48428d89ae9f29ff77b643a26aa046fe
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-26 12:37:52 -0500

    2 parte ya final muestra en la tabla

--- frondend/pages/clientes/clientes.js
+++ frondend/pages/clientes/clientes.js
@@ -9,14 +9,17 @@ async function cargaClientes(){
     console.log(clientes);
     const tablaClientes = document.querySelector("#datosClientes")
     clientes.forEach(element=>{
-        tablaClientes.innerHTML =`
+        const {id_constructora,nombre_constructora,nit_constructora,nombre_representante,email_contacto,telefono_contacto} = element
+        tablaClientes.innerHTML +=`
                           
         <tr>
-        <th scope="row">id</th>
-        <td>Constructora</td>
-        <td>NombreRespresentante</td>
-        <td>gmail</td>
-        <td>telefono</td>
+        <th scope="row">${id_constructora}</th>
+        <td>${nombre_representante}</td>
+        <td>${nit_constructora}</td>
+        <td>${nombre_constructora}</td>
+        <td>${email_contacto}</td>
+        <td>${telefono_contacto}</td>
+        
       </tr>
       `
     })
