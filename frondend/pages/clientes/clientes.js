89d256a8b7ff90e62913ac79dbfaac8376187002
JairoCardenas123 <CardenasJairo102011@gmail.com>
2023-06-26 12:33:32 -0500

    para poder mostrarlos en la tabla

--- frondend/pages/clientes/clientes.js
+++ frondend/pages/clientes/clientes.js
@@ -8,4 +8,16 @@ async function cargaClientes(){
     const clientes = await getClientes();
     console.log(clientes);
     const tablaClientes = document.querySelector("#datosClientes")
+    clientes.forEach(element=>{
+        tablaClientes.innerHTML =`
+                          
+        <tr>
+        <th scope="row">id</th>
+        <td>Constructora</td>
+        <td>NombreRespresentante</td>
+        <td>gmail</td>
+        <td>telefono</td>
+      </tr>
+      `
+    })
 }
